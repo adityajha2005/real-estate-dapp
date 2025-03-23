4505fc3d910d2237e4967c24ab99460b7e67593a
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-03-23 11:16:42 +0530

    metamask connection established

--- src/App.js
+++ src/App.js
@@ -15,12 +15,26 @@ import config from './config.json';
 
 function App() {
 
+  const[account,setAccount]= useState(null)
+  const loadBlockchainData = async()=>{
+    const providers = new ethers.providers.Web3Provider(window.ethereum)
+    // console.log(providers)
+    // const accounts = await window.ethereum.request({
+    //   method:'eth_requestAccounts'
+    // })
+    // setAccount(accounts[0])
+    // console.log(accounts[0])
+    // await window.ethereum
+  }
+  useEffect(()=>{
+    loadBlockchainData()
+  },[])
   return (
     <div>
-
+      <Navigation account={account} setAccount={setAccount} />   
       <div className='cards__section'>
 
-        <h3>Welcome to Millow</h3>
+        <h3>Welcome to DApp!</h3>
 
       </div>
 
